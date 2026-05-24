c430e221fb0193074bc6ad6bd2163e590364399f
Thomas <thomas@keepertax.com>
2026-05-24 14:39:29 -0700

    Add fungible formula v1.0.0

---
+++ Formula/fungible.rb
@@ -0,0 +1,26 @@
+class Fungible < Formula
+  desc "Terminal personal finance manager with Plaid sync and AI categorization"
+  homepage "https://github.com/tomfunk/fungible"
+  url "https://github.com/tomfunk/fungible/archive/refs/tags/v1.0.0.tar.gz"
+  sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+  license "ISC"
+
+  depends_on "node"
+
+  def install
+    system "npm", "ci", "--omit=dev"
+    libexec.install Dir["*"]
+    bin.install_symlink libexec/"bin/fungible"
+  end
+
+  def caveats
+    <<~EOS
+      Run `fungible --setup` on first launch to configure Plaid credentials.
+      Data and config are stored in ~/.fungible/
+    EOS
+  end
+
+  test do
+    assert_predicate bin/"fungible", :executable?
+  end
+end
